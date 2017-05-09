1922be38ed1bf4e8a8f3861515b24adfef9316ed
Florian Tönjes <skatepainter@yahoo.de>
2017-05-09 11:51:12 +0200

    Add minifyScripts task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@
 
 const gulp = require("gulp"),
       concat = require("gulp-concat"),
-      maps = require("gulp-sourcemaps");
+      maps = require("gulp-sourcemaps"),
+      uglify = require("gulp-uglify"),
+      rename = require("gulp-rename");
 
-gulp.task("scripts", function () {
+gulp.task("concatScripts", function () {
     return gulp.src([
         "./js/*.js",
         "./js/circle/*.js"
@@ -12,5 +14,12 @@ gulp.task("scripts", function () {
     .pipe(maps.init())
     .pipe(concat("app.js"))
     .pipe(maps.write("./"))
+    .pipe(gulp.dest("js"));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function () {
+    return gulp.src("js/app.js")
+    .pipe(uglify())
+    .pipe(rename("app.min.js"))
     .pipe(gulp.dest("js"));
 });
